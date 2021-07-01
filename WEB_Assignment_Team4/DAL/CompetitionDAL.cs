496addae993763c06382c819abcc62f40284c7a8
toyocrabs <mljkprvt@gmail.com>
2021-07-01 23:16:23 +0800

    started on the main voting page for the public

---
+++ WEB_Assignment_Team4/DAL/CompetitionDAL.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEB_Assignment_Team4.DAL
+{
+    public class CompetitionDAL
+    {
+    }
+}
---
+++ WEB_Assignment_Team4/Models/Competition.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WEB_Assignment_Team4.Models
+{
+    public class Competition
+    {
+        [Display(Name = "Competition ID")]
+        public int CompetitionID { get; set; }
+
+        [Display(Name = "Interest ID")]
+        public int AreaInterestID { get; set; }
+
+        [Display(Name = "Competition Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Start Date")]
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+
+        [Display(Name = "End Date")]
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+
+        [Display(Name = "Result Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ResultReleaseDate { get; set; }
+    }
+}
