8c1248213f8b1575c7abb3d04f363b76cd42d60b
toyocrabs <mljkprvt@gmail.com>
2021-07-01 23:48:33 +0800

    Added competitionDAL

--- WEB_Assignment_Team4/DAL/CompetitionDAL.cs
+++ WEB_Assignment_Team4/DAL/CompetitionDAL.cs
@@ -2,10 +2,65 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Data.SqlClient;
+using WEB_Assignment_Team4.Models;
 
 namespace WEB_Assignment_Team4.DAL
 {
     public class CompetitionDAL
     {
+        private IConfiguration Configuration { get; }
+        private SqlConnection conn;
+
+        public CompetitionDAL()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+            string strConn = Configuration.GetConnectionString("NPBookConnectionString");
+
+            //Instantiate a Sqlconnection object with the
+            //Connection String read.
+            conn = new SqlConnection(strConn);
+        }
+
+        public List<Competition> GetAllCompetition()
+        {
+            //Create a SqlCommand object from connection object 
+            SqlCommand cmd = conn.CreateCommand();
+            //Specify the SELECT SQL statments
+            cmd.CommandText = @"SELECT * FROM Competition ORDER BY CompetitionID";
+            //Open a database connection
+            conn.Open();
+            //Execute the SELECT SQL through a DataReader
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            //Read all records until the end, save data into a staff list
+            List<Competition> competitionList = new List<Competition>();
+            while (reader.Read())
+            {
+                competitionList.Add(
+                    new Competition
+                    {
+                        CompetitionID = reader.GetInt32(0),
+                        AreaInterestID = reader.GetInt32(1),
+                        Name = reader.GetString(2),
+                        StartDate = reader.GetDateTime(3),
+                        EndDate = reader.GetDateTime(4),
+                        ResultReleaseDate = reader.GetDateTime(5),
+                    }
+                );
+            }
+            //Close DataReader
+            reader.Close();
+            //Close the database connection
+            conn.Close();
+
+            return competitionList;
+        }
     }
 }
