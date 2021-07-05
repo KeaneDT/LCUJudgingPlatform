e6d70cfcc2843207e1225c97b8de1c94303fbf85
Lim Jin Hui <s10208093@connect.np.edu.sg>
2021-07-05 16:42:19 +0800

    added a view page to see the competiton records.

---
+++ WEB_Assignment_Team4/Controllers/CompetitionController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WEB_Assignment_Team4.DAL;
+using WEB_Assignment_Team4.Models;
+
+namespace WEB_Assignment_Team4.Controllers
+{
+    public class CompetitionController : Controller
+    {
+        private CompetitionDAL competitionContext = new CompetitionDAL();
+        
+        // GET
+        public ActionResult Index()
+        {
+            //Stop Accessing the action if not logged in
+            //or account not in the "Administrator" role
+            if (HttpContext.Session.GetString("Role") == null ||
+              (HttpContext.Session.GetString("Role") != "Administrator"))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            List<Competition> competitionList = competitionContext.GetAllCompetition();
+            return View(competitionList);
+        }
+
+        // GET: CompetitionController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CompetitionController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CompetitionController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CompetitionController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CompetitionController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CompetitionController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CompetitionController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
