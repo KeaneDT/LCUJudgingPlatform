47251e555208e808fa75c51df938465097e59789
KeaneDT <S10202630@connect.np.edu.sg>
2021-07-05 17:14:12 +0800

    Added GetSalutations & GetAllInterest to JudgeController

---
+++ WEB_Assignment_Team4/Controllers/JudgeController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WEB_Assignment_Team4.DAL;
+using WEB_Assignment_Team4.Models;
+
+namespace WEB_Assignment_Team4.Controllers
+{
+    public class JudgeController : Controller
+    {
+        private InterestDAL interestContext = new InterestDAL();
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            // Stop accessing the action if not logged in
+            // or account not in the "Staff" role
+            if ((HttpContext.Session.GetString("Role") == null) ||
+                (HttpContext.Session.GetString("Role") != "Judge"))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            ViewData["SalutationList"] = GetSalutations();
+            ViewData["InterestList"] = GetAllInterest();
+            return View();
+        }
+        private List<SelectListItem> GetSalutations()
+        {
+            List<SelectListItem> sal = new List<SelectListItem>();
+            sal.Add(new SelectListItem
+            {
+                Value = "Dr",
+                Text = "Dr"
+            }); sal.Add(new SelectListItem
+            {
+                Value = "Mr",
+                Text = "Mr"
+            }); sal.Add(new SelectListItem
+            {
+                Value = "Ms",
+                Text = "Ms"
+            }); sal.Add(new SelectListItem
+            {
+                Value = "Mrs",
+                Text = "Mrs"
+            }); sal.Add(new SelectListItem
+            {
+                Value = "Mdm",
+                Text = "Mdm"
+            });
+
+            return sal;
+        }
+        private List<Interest> GetAllInterest()
+        {
+            // Get a list of branches from database
+            List<Interest> interestList = interestContext.GetAllInterest();
+            // Adding a select prompt at the first row of the branch list
+            interestList.Insert(0, new Interest
+            {
+                AreaInterestID = 0,
+                Name = "--Select--"
+            });
+            return interestList;
+        }
+    }
+}
