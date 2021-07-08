ea11c7161177e8a835d94476ef855e4dda901761
toyocrabs <mljkprvt@gmail.com>
2021-07-09 00:33:12 +0800

    Started on the view submissions of competitions page, added submissions model and controller

---
+++ WEB_Assignment_Team4/Models/Submissions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WEB_Assignment_Team4.Models
+{
+    public class Submissions
+    {
+        [Display(Name = "CompetitionID")]
+        public int CompetitionID { get; set; }
+
+        [Display(Name = "CompetitorID")]
+        public int CompetitorID { get; set; }
+
+        [Display(Name = "File Name")]
+        public string FileName { get; set; }
+
+        [Display(Name = "Date and time of file upload")]
+        [DataType(DataType.Date)]
+        public DateTime UploadDateTime { get; set; }
+
+        [Display(Name = "Total votes")]
+        public int VoteCount { get; set; }
+
+        [Display(Name = "Ranking")]
+        public int Ranking { get; set; }
+    }
+}
