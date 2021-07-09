ae1a6addb1294a7f5fb3057544a68afa615577d7
KeaneDT <S10202630@connect.np.edu.sg>
2021-07-09 08:54:54 +0800

    Created Criteria Model & CompetitionCriteriaViewModel

---
+++ WEB_Assignment_Team4/Models/CompetitionCriteriaViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEB_Assignment_Team4.Models
+{
+    public class CompetitionCriteriaViewModel
+    {
+        public List<Competition> competitionList { get; set; }
+        public List<Criteria> criteriaList { get; set; }
+
+        public CompetitionCriteriaViewModel()
+        {
+            competitionList = new List<Competition>();
+            criteriaList = new List<Criteria>();
+        }
+    }
+}
---
+++ WEB_Assignment_Team4/Models/Criteria.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WEB_Assignment_Team4.Models
+{
+    public class Criteria
+    {
+        //CriteriaID
+        public int CriteriaID { get; set; }
+        //CompetitionID
+        public int CompetitionID { get; set; }
+        //CriteriaName
+        [Required(ErrorMessage = "Criteria Name is Required!")]
+        [StringLength(50, ErrorMessage = "Judge Name cannot be more than 50 characters!")]
+        public string CriteriaName { get; set; }
+        //Weightage
+        [Range(1,100, ErrorMessage = "Criteria Weightage cannot be the integer specified!")]
+        public int Weightage { get; set; }
+    }
+}
