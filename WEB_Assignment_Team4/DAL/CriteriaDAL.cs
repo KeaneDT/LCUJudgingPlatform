328d0cfa10eae22c14ef5210d534d7e6f4e7756f
KeaneDT <S10202630@connect.np.edu.sg>
2021-07-09 09:07:33 +0800

    Created CriteriaDAL & Added GetCompetitionCriteria and Add Methods

---
+++ WEB_Assignment_Team4/DAL/CriteriaDAL.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Data.SqlClient;
+using WEB_Assignment_Team4.Models;
+
+namespace WEB_Assignment_Team4.DAL
+{
+    public class CriteriaDAL
+    {
+        private IConfiguration Configuration { get; set; }
+        private SqlConnection conn;
+        //Constructor
+        public CriteriaDAL()
+        {
+            //Read ConnectionString from appsettings.json file
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+            string strConn = Configuration.GetConnectionString(
+                "CJPConnectionString");
+
+            //Instantiate a SqlConnection object with the
+            //Connection String read.
+            conn = new SqlConnection(strConn);
+        }
+        public List<Criteria> GetCompetitionCriteria(int compID)
+        {
+            //Create a SqlCommand object from connection object 
+            SqlCommand cmd = conn.CreateCommand();
+            //Specify the SELECT SQL statments
+            cmd.CommandText = @"SELECT * FROM Criteria WHERE CompetitionID = @competitionID";
+            cmd.Parameters.AddWithValue("@competitionID", compID);
+
+            //Open a database connection
+            conn.Open();
+            //Execute the SELECT SQL through a DataReader
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            //Read all records until the end, save data into a staff list
+            List<Criteria> cList = new List<Criteria>();
+            while (reader.Read())
+            {
+                cList.Add(
+                    new Criteria
+                    {
+                        CriteriaID = reader.GetInt32(0),
+                        CompetitionID = reader.GetInt32(1),
+                        CriteriaName = reader.GetString(2),
+                        Weightage = reader.GetInt32(3),
+                    }
+                );
+            }
+            //Close DataReader
+            reader.Close();
+            //Close the database connection
+            conn.Close();
+
+            return cList;
+        }
+        public int Add(Criteria criteria)
+        {
+            SqlCommand cmd = conn.CreateCommand();
+
+            cmd.CommandText = @"INSERT INTO Criteria (CompetitionID, CriteriaName, Weightage)
+                                OUTPUT INSERTED.CriteriaID
+                                VALUES(@competitionID, @cName, @weightage)";
+
+            cmd.Parameters.AddWithValue("@competitionID", criteria.CompetitionID);
+            cmd.Parameters.AddWithValue("@cName", criteria.CriteriaName);
+            cmd.Parameters.AddWithValue("@weightage", criteria.Weightage);
+
+            conn.Open();
+            criteria.CriteriaID = (int)cmd.ExecuteScalar();
+            conn.Close();
+
+            return criteria.CriteriaID;
+        }
+    }
+}
