782eb7fde3410f853285533d0bb7831873191087
toyocrabs <mljkprvt@gmail.com>
2021-07-25 20:55:05 +0800

    added comments model

---
+++ WEB_Assignment_Team4/Models/Comment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WEB_Assignment_Team4.Models
+{
+    public class Comment
+    {
+        [Display(Name = "Comment ID")]
+        public int CommentID { get; set; }
+
+        [Display(Name = "Competition ID")]
+        public int CompetitionID { get; set; }
+
+        [Display(Name = "Comment")]
+        public string Description { get; set; }
+
+        [Display(Name = "Date And Time Posted")]
+        [DataType(DataType.Date)]
+        public DateTime? DateTimePosted { get; set; }
+    }
+}
