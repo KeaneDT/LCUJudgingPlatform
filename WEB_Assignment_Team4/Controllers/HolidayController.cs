53ee34d6356ce535849d948b499bcb770c492cc6
KeaneDT <S10202630@connect.np.edu.sg>
2021-07-29 10:05:11 +0800

    Attempt to add Holiday API

---
+++ WEB_Assignment_Team4/Controllers/HolidayController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WEB_Assignment_Team4.Models;
+
+namespace WEB_Assignment_Team4.Controllers
+{
+    public class HolidayController : Controller
+    {
+        // GET: HolidayController
+        public async Task<ActionResult> Index()
+        {
+            HttpClient client = new HttpClient();
+            string year = DateTime.Today.Year.ToString("YYYY");
+            string month = DateTime.Today.Month.ToString("MM");
+            string day = DateTime.Today.Day.ToString("dd");
+            client.BaseAddress = new Uri("https://holidays.abstractapi.com/v1/?api_key=bd9022d26baf47dd8ddba77582ba3e24&country=SG&year=2021&month=12&day=25");
+            HttpResponseMessage response = await client.GetAsync("");
+            if (response.IsSuccessStatusCode)
+            {
+                string data = await response.Content.ReadAsStringAsync();
+                List<Holiday> holList = JsonConvert.DeserializeObject<List<Holiday>>(data);
+                return View(holList);
+            }
+            else
+            {
+                return View(new List<Holiday>());
+            }
+        }
+
+        // GET: HolidayController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: HolidayController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: HolidayController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HolidayController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: HolidayController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HolidayController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: HolidayController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WEB_Assignment_Team4/Models/Holiday.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEB_Assignment_Team4.Models
+{
+    public class Holiday
+    {
+        public string Name { get; set; }
+        public string Name_local { get; set; }
+        public string Language { get; set; }
+        public string Description { get; set; }
+        public string Country { get; set; }
+        public string Location { get; set; }
+        public string Type { get; set; }
+        public string Date { get; set; }
+        public string Date_year { get; set; }
+        public string Date_month { get; set; }
+        public string Date_day { get; set; }
+        public string Week_day { get; set; }
+    }
+}
