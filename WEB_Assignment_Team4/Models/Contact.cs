029db6123f749c42021f1bed4ba46c7970cc5f22
Lim Jin Hui <s10208093@connect.np.edu.sg>
2021-07-29 21:58:02 +0800

    added contact model.

---
+++ WEB_Assignment_Team4/Models/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEB_Assignment_Team4.Models
+{
+
+    public class Rootobject
+    {
+        public Contact[] Property1 { get; set; }
+    }
+
+    public class Contact
+    {
+        public string _id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string Message { get; set; }
+        public string PhoneNumber { get; set; }
+        public string SubjectTitle { get; set; }
+    }
+}
